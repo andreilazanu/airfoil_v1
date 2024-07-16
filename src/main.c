be58da70168ed70392e4eeeb8a1a66bed08329e4
andreilazanu <100133534+andreilazanu@users.noreply.github.com>
2024-07-16 10:37:31 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,24 +1,114 @@
 #include <stdio.h>
 #include <uxhw.h>
+#include <math.h>
 
-int
-main(int argc, char *  argv[])
+
+# define A0 661.47  //the speed of sound at standard sea level (661.47 knots (1,225.04 km/h; 340.29 m/s)),
+# define T0 288.15 // (the temperature at standard sea level in K)
+# define RHO0 1.225 // the air density at sea level in the International Standard Atmosphere (15 °C and 1013.25 hectopascals, corresponding to a density of 1.225 kg/m3),
+# define P0 1.01325e5 // pressure at sea level
+
+
+static void
+loadInputs(double *  p, double *  ps, double *  T, double *  A, double *  alpha, double *  pr, double *  W, double *  beta)
 {
-	double	a, b, c;
 
-	a = UxHwDoubleUniformDist(0.5, 1.0);
-	printf("a = %lf\n", a);
+    //*M        = UxHwDoubleDistFromSamples(empiricalTaylorFactorValues, sizeof(empiricalTaylorFactorValues)/sizeof(double));
+    *p      = UxHwDoubleUniformDist(8.e4*(1.-0.22/100), 8.e4*(1+0.22/100));
+    *ps     = UxHwDoubleUniformDist(5.e4*(1.-0.22/100), 5.e4*(1+0.22/100));
+    *T      = UxHwDoubleUniformDist(260.*(1.-0.15/100), 260.*(1.+0.15/100));
+    *A      = 1000.;	
+    *alpha  = UxHwDoubleUniformDist(4*M_PI/180.*(1.-0.15/100), 4*M_PI/180.*(1.+0.15/100));
+    *pr     = UxHwDoubleUniformDist(0.06*(1.-0.15/100), 0.06*(1.+0.15/100));
+    *W      = UxHwDoubleUniformDist(25.*(1.-0.15/100), 25.*(1.+0.15/100));
+    *beta   = UxHwDoubleUniformDist(30*M_PI/180..*(1.-0.15/100), 30*M_PI/180.*(1.+0.15/100));
+}
 
-	b = UxHwDoubleUniformDist(10.0, 20.0);
-	printf("b = %lf\n", b);
 
-	c = (a+b)/(a-b);
-	printf("c = %lf\n", c);
 
-#ifdef DEBUG
-	printf("debug message\n");
-#endif
 
-	return 0;
+    //Airfoil
+
+    double alpha=4*M_PI/180.;
+    double pr=0.06;
+    //Wind
+    double W=25;
+    double beta=30*M_PI/180.;
+
+
+double coeff1(double percent, double alpha) {
+    // lift coefficient
+    double C0=2.0*M_PI*(2.0*percent+1-sqrt(4.0*percent*percent+1));
+    return C0+2.0*M_PI*alpha;
+}
+
+
+
+double rho(double p, double T) {
+    return RHO0*p*T0/(P0*T);
+}
+
+double tas(double p, double ps, double T) {
+    // This is the true air speed
+    // https://en.wikipedia.org/wiki/True_airspeed
+
+    double tassubsonic=sqrt(2.*(p-ps)/p*T/(RHO0*T0));;
+    double tassupersonic=A0*sqrt(T/T0)*sqrt(5.*pow((p-ps)/p+1,(2./7.))-1.);;
+    
+   if (tassubsonic<340)
+       return tassubsonic;
+    else
+        return tassupersonic;
+}
+
+double lift(double c1, double density, double v, double A) {
+    // lift formula
+    return c1*density*v*v*A/2.0;
+}
+
+
+// Model parameters p, p_s, T
+
+int main(int argc, const char * argv[]) {
+/*
+    //Atmosphere
+    double p=8.e4;
+    double ps=5.e4;
+    double T=260;
+
+    //Airfoil
+    double A=1000;
+    double alpha=4*M_PI/180.;
+    double pr=0.06;
+	
+    //Wind
+    double W=25;
+    double beta=30*M_PI/180.;
+*/
+	
+    double p;
+    double ps;
+    double T;
+
+    //Airfoil
+    double A;
+    double alpha;
+    double pr;
+	
+    //Wind
+    double W;
+    double beta;
+
+    loadInputs(&p, &ps, &T, &A, &alpha, &pr, &W, &beta);
+	
+    double c1=coeff1(pr,alpha);
+    double density=rho(p,T);
+    
+
+    
+	
+    double v=tas(p,ps,T)*cos(alpha)+W*cos(beta);
+    printf("Lift force is %e \n",lift(c1,density,v,A));
+    return 0;
 }
 
