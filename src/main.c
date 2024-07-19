ece0aa68b8de0ccdbffa6a83ce6578309dae0681
andreilazanu <100133534+andreilazanu@users.noreply.github.com>
2024-07-19 18:36:02 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,13 +10,13 @@
 
 
 static void
-loadInputs(double *  p, double *  ps, double *  T, double *  A, double *  alpha, double *  pc, double *  W, double *  beta)
+loadInputs(double *  p, double *  ps, double *  T, double *  S, double *  alpha, double *  pc, double *  W, double *  beta)
 {
 
     *p      = UxHwDoubleUniformDist(8.e4*(1.-0.22/100.), 8.e4*(1+0.22/100.));
     *ps     = UxHwDoubleUniformDist(5.e4*(1.-0.22/100.), 5.e4*(1+0.22/100.));
     *T      = UxHwDoubleUniformDist(275.*(1.-0.15/100.), 275.*(1.+0.15/100.));
-    *A      = 1000.;	
+    *S      = 1000.;	
     *alpha  = UxHwDoubleUniformDist(4.*M_PI/180.*(1.-8./100.), 4.*M_PI/180.*(1.+8./100.));
     *pc     = 0.06;
     *W      = UxHwDoubleUniformDist(25.*(1.-2./100.), 25.*(1.+2./100.));
@@ -24,13 +24,13 @@ loadInputs(double *  p, double *  ps, double *  T, double *  A, double *  alpha,
 }
 
 static void
-loadInputs2(double *  p, double *  ps, double *  T, double *  A, double *  alpha, double *  pc, double *  W, double *  beta)
+loadInputs2(double *  p, double *  ps, double *  T, double *  S, double *  alpha, double *  pc, double *  W, double *  beta)
 {
 
     *p      = UxHwDoubleUniformDist(5.5e4*(1.-0.22/100.), 5.5e4*(1+0.22/100.));
     *ps     = UxHwDoubleUniformDist(5.03e4*(1.-0.22/100.), 5.03e4*(1+0.22/100.));
     *T      = UxHwDoubleUniformDist(258.*(1.-0.15/100.), 258.*(1.+0.15/100.));
-    *A      = 1000.;	
+    *S      = 1000.;	
     *alpha  = UxHwDoubleUniformDist(6.*M_PI/180.*(1.-8./100.), 6.*M_PI/180.*(1.+8./100.));
     *pc     = 0.04;
     *W      = UxHwDoubleUniformDist(15.*(1.-2./100.), 15.*(1.+2./100.));
@@ -83,7 +83,7 @@ int main(int argc, const char * argv[]) {
     double T;
 
     //Airfoil
-    double A;
+    double S;
     double alpha;
     double pc;
 	
@@ -96,18 +96,18 @@ int main(int argc, const char * argv[]) {
     double v;
 
     // Model 1
-    loadInputs(&p, &ps, &T, &A, &alpha, &pc, &W, &beta);
+    loadInputs(&p, &ps, &T, &S, &alpha, &pc, &W, &beta);
     c1=coeff1(pc,alpha);
     density=rho(p,T);
     v=tas(p,ps,T)+W*cos(beta-alpha);
-    printf("Lift force (1) is %e \n",lift(c1,density,v,A,p,ps));
+    printf("Lift force (1) is %e \n",lift(c1,density,v,S,p,ps));
 
     // Model 2
-    loadInputs2(&p, &ps, &T, &A, &alpha, &pc, &W, &beta);
+    loadInputs2(&p, &ps, &T, &S, &alpha, &pc, &W, &beta);
     c1=coeff1(pc,alpha);
     density=rho(p,T);
     v=tas(p,ps,T)+W*cos(beta-alpha);
-    printf("Lift force (2) is %e \n",lift(c1,density,v,A,p,ps));
+    printf("Lift force (2) is %e \n",lift(c1,density,v,S,p,ps));
 
 
 	
