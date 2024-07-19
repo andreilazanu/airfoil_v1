f123b7a6ab714e126b11b884374f8613ee655291
andreilazanu <100133534+andreilazanu@users.noreply.github.com>
2024-07-19 12:57:01 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -66,7 +66,7 @@ double mach(double p, double ps) {
 double lift(double c1, double density, double v, double A, double p, double ps) {
     // lift formula
     double m=mach(p,ps);
-    print("Mach %f\n",m);
+    printf("Mach %f\n",m);
     if(m<0.5)
 	    return c1*(p-ps)*A/sqrt(1.-m*m);
     else
